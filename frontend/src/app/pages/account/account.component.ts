0ff2e3d75b29cfd31d2780786387f82eb3c4a692
OanaMB <oana_maria.bacaran@stud.acs.upb.ro>
2025-01-17 03:09:22 +0200

    updates

--- frontend/src/app/pages/account/account.component.ts
+++ frontend/src/app/pages/account/account.component.ts
@@ -1,4 +1,10 @@
+// Updated account.component.ts
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ScannerDialogComponent } from '../scanner-dialog/scanner-dialog.component';
+import { AuthGuard } from 'src/app/guards/auth.guard';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-account',
@@ -6,10 +12,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent {
-    account = {
-        name: 'John Doe',
-        username: 'JohnDoe',
-        email: 'john.doe@example.com',
-        password: 'blublublu (ceva ceva criptat)'
-      };
+  userData: any = {
+    email: String,
+    memberSince: new Date('2025-01-17'),
+    points: 75,
+    rewards: [
+      { title: 'Reward 1', image: 'reward1.png' },
+      { title: 'Reward 2', image: 'reward2.png' }
+    ]
+  };
+
+  // userInitials: string = this.userData.email
+  //   .split(' ')
+  //   .map((n: string) => n[0])
+  //   .join('');
+  userInitials: string;
+
+  constructor(private dialog: MatDialog, private authService: AuthService) {
+    this.userData.email = this.authService.getEmail();
+    this.userInitials = this.userData.email
+    .split(' ')
+    .map((n: string) => n[0])
+    .join('');
+  }
+
+  openScannerDialog(): void {
+    const dialogRef = this.dialog.open(ScannerDialogComponent, {
+      width: '400px',
+      height: '600px',
+      disableClose: true, // Prevent closing when clicking outside
+      panelClass: 'scanner-dialog-panel' // Optional custom styling
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('Scanner dialog closed', result);
+    });
+  }
 }
